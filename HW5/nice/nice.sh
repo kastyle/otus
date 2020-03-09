312f91f150448347932e49385d785239c74457bb
kastyle <48478298+kastyle@users.noreply.github.com>
2020-03-09 21:22:23 +0300

    Update nice.sh

--- HW5/nice/nice.sh
+++ HW5/nice/nice.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "high and low priority test started at $(date)" > log.txt
+echo "high and low priority test started at $(date)" > nicelog.txt
 
 proc1 () {
     nice -n 19 dd if=/dev/urandom of=/dev/null bs=128M count=2000
-    echo "low priority test stopped at $(date)" >> log.txt
+    echo "low priority test stopped at $(date)" >> nicelog.txt
 }
 
 proc2 () {
     nice -n -20 dd if=/dev/urandom of=/dev/null bs=128M count=2000
-    echo "high priority test stopped at $(date)" >> log.txt
+    echo "high priority test stopped at $(date)" >> nicelog.txt
 }
 
 proc1 &
